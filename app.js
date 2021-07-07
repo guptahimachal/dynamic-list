f8a8334388fd2147fc879a688b13b44974bf56f0
guptahimachal <guptahimachal@gmail.com>
2021-07-07 18:08:30 +0530

    Setting up list.ejs setting to do list fucntionaity

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs');
 
 // For Post
 app.use(express.urlencoded({extended:true}))
 
+app.use(express.static('public'))
+
+let newListItems = ["Buy food","Cook food","Eat food"];
+
 app.get('/',(req,res)=>{
     console.log("Request received from client")
     
-    var today = new Date();
-    if(today.getDay() === 6 || today.getDay === 7 )
+    let today = new Date();
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    }
+    // var day = today.toLocaleDateString("hi-u-ca-indian",options)
+    let day = today.toLocaleDateString("en-BZ",options)
     
-    
-    
-    res.send("Home Page")
+    res.render('list',{kindOfDay : day, newListItemsH : newListItems})
+})
+
+app.post('/',(req,res)=>{
+    newListItems.push(req.body.newItem)
+    res.redirect('/')
 })
 
 app.listen(5000,()=>{
