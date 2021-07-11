a7a0502d5712127c0ca0f79741e880f1dd636860
guptahimachal <guptahimachal@gmail.com>
2021-07-12 00:20:26 +0530

    Adding and deleting items from custom lisr

--- app.js
+++ app.js
@@ -10,15 +10,24 @@ app.use(express.static('public'))
 
 // Setting up mongoogse for DB integration
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false});
 
 // Creating the schema for list items
 const itemSchema = new mongoose.Schema({
     name: String,
 });
-
 // Creating the model(collection) for list items
 const Item = mongoose.model('item', itemSchema);
+
+
+// Creating the schema for storing custom list
+const listSchema = new mongoose.Schema({
+    name : String,
+    items : [itemSchema],
+});
+// Ctrating the model(collection) for storing custom lists
+const List = mongoose.model('list', listSchema);
+
 
 // newListItems contains the default items to be displayed in the list
 let newListItems = ["Buy food","Cook food","Eat food"];
@@ -48,44 +57,88 @@ app.get('/',(req,res)=>{
                 res.redirect("/");
             }
             else // If the list is not empty, then display the items
-                res.render('list',{listTitle : day, newListItemsH : items})
+                res.render('list',{listTitle : "Today", newListItemsH : items})
         }
     });
 })
 
 app.post('/',(req,res)=>{
     const itemName = req.body.newItem;
+    const listName = req.body.list;
     let item = new Item({name:itemName});
-    item.save();
-    res.redirect("/");
 
-    // if(req.body.list === 'Work List'){
-    //     workItems.push(item)
-    //     res.redirect('/work')
-    // }
-    // else{
-    //     newListItems.push(item)
-    //     res.redirect('/')
-    // }
-})
+    if(listName === "Today"){
+        item.save();
+        res.redirect("/");
+    }
+    else{
+        List.findOne({name:listName},(err,list)=>{
+            if(!err)
+                if(list){
+                    list.items.push(item);
+                    list.save();
+                    res.redirect("/"+listName);
+                }
+        });
+    }
+});
 
 app.post('/delete',(req,res)=>{
     // itemToBeDeleted is array containing _id of the items selected in checkbox
     let itemToBeDeleted = req.body.itemID;
-    Item.deleteMany({ _id: {
+    const listName = req.body.list;
+
+    if(listName === 'Today'){
+        Item.deleteMany({ _id: {
             $in : itemToBeDeleted
         }
-    }, (err,result)=>{
-        if(err)
-            console.log(err)
-        else
-            console.log(result.deletedCount,"items deleted")
-    });
-    res.redirect('/');
-});
+        }, (err,result)=>{
+            if(err)
+                console.log(err)
+            else
+                console.log(result.deletedCount,"items deleted")
+        });
+        res.redirect('/');
+    }
+    else{
+        List.findOneAndUpdate(
+            {  name: listName },
+            { $pull:
+                {
+                    items: 
+                    { 
+                        _id:
+                        {
+                            $in:itemToBeDeleted
+                        }
+                    }
+                } 
+            },
+            (err,result)=>{
+                if(!err)
+                    res.redirect("/"+listName);
+                else
+                    console.log(err)
+            }
+        )
+    }
+});      
 
-app.get('/work',(req,res)=>{
-    res.render("list",{listTitle: "Work List",newListItemsH : workItems})
+app.get('/:listName',(req,res)=>{
+    const listName = req.params.listName;
+    
+    List.findOne({name:listName},(err,list)=>{
+        if(!err)
+            if(list){
+                let items = list.items;
+                res.render('list',{listTitle : listName,  newListItemsH : items})
+            }
+            else{
+                const newList = new List({name : listName, items : defaultItems});
+                newList.save();
+                res.redirect("/"+listName);
+            }
+    });
 })
 
 app.listen(5000,()=>{
